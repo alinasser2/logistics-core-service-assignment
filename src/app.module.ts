97feaacffe9c4ba4489f70107b31046c977d393f
arsany98 <arsanymaged13@gmail.com>
2025-05-02 20:14:08 +0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+      type: 'sqlite',
+      autoLoadEntities: true,
+      synchronize: false,
+  })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
