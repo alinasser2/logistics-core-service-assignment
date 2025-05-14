e6411191f8af9f63a476cf55cc4cb87a58c91d98
alinasser2 <alinasserelden@hotmail.com>
2025-05-14 17:47:47 +0300

    Refactor Shipment entity to include missing decorators and improve indexing

--- src/shipment/entities/shipment.entity.ts
+++ src/shipment/entities/shipment.entity.ts
@@ -5,29 +5,38 @@ import {
   ManyToOne,
   CreateDateColumn,
   UpdateDateColumn,
+  Index,
+  DeleteDateColumn,
 } from 'typeorm';
 import { Status } from './status.entity';
 
-@Entity()
+@Entity('shipments')
 export class Shipment {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column({ name: 'tracking_id', unique: true })
+  @Index()
   trackingId: string;
 
   @Column({ name: 'phone_number' })
+   @Index()
   phoneNumber: string;
 
   @Column()
   description: string;
 
   @ManyToOne(() => Status, { eager: true })
+  @Index() 
   status: Status;
 
   @CreateDateColumn({ name: 'creation_date' })
+  @Index() 
   createdAt: Date;
 
   @UpdateDateColumn({ name: 'modification_date' })
   updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt?: Date;
 }
