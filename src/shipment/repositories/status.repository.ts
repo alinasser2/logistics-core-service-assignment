da0652440e7421adc47da5fe5be5bb1e287bee93
alinasser2 <alinasserelden@hotmail.com>
2025-05-14 14:10:13 +0300

    Add StatusRepository for managing status data and operations

---
+++ src/shipment/repositories/status.repository.ts
@@ -0,0 +1,17 @@
+// repositories/status.repository.ts
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Status } from '../entities/status.entity';
+
+@Injectable()
+export class StatusRepository {
+  constructor(
+    @InjectRepository(Status)
+    private readonly repo: Repository<Status>,
+  ) {}
+
+  findByName(name: string) {
+    return this.repo.findOne({ where: { name } });
+  }
+}
