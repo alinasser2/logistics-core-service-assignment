907b7fee259df1acc7bd91b0d2cb322a33308fbf
alinasser2 <alinasserelden@hotmail.com>
2025-05-14 17:47:50 +0300

    Refactor Status entity to specify table name and add missing index decorator

--- src/shipment/entities/status.entity.ts
+++ src/shipment/entities/status.entity.ts
@@ -1,12 +1,13 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, Index } from 'typeorm';
 import { Shipment } from './shipment.entity';
 
-@Entity()
+@Entity('statuses')
 export class Status {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column({ unique: true })
+  @Index()
   name: string;
 
   @OneToMany(() => Shipment, shipment => shipment.status)
