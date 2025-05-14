53fcf8335036124502d6095df92352aab1b81fc2
alinasser2 <alinasserelden@hotmail.com>
2025-05-14 14:09:53 +0300

    Add ShipmentRepository and StatusRepository to ShipmentModule providers

--- src/shipment/shipment.module.ts
+++ src/shipment/shipment.module.ts
@@ -3,11 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Shipment } from './entities/shipment.entity';
 import { Status } from './entities/status.entity';
 import { ShipmentService } from './services/shipment.service';
+import { ShipmentRepository } from './repositories/shipment.repository';
+import { StatusRepository } from './repositories/status.repository';
 import { ShipmentController } from './controllers/shipment.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Shipment, Status])],
-  controllers: [ShipmentController],
-  providers: [ShipmentService],
+  controllers: [ShipmentController], 
+  providers: [ShipmentService, ShipmentRepository, StatusRepository],
+  exports: [ShipmentService],
 })
 export class ShipmentModule {}
