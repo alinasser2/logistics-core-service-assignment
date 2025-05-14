837c6027ede22bc7ec80a7be7ebbc6077f182a74
alinasser2 <alinasserelden@hotmail.com>
2025-05-14 08:51:04 +0300

    Add ApiProperty decorators to CreateShipmentDto for improved Swagger documentation

--- src/shipment/dto/create-shipment.dto.ts
+++ src/shipment/dto/create-shipment.dto.ts
@@ -1,15 +1,19 @@
 import { IsNotEmpty, IsPhoneNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateShipmentDto {
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({ example: '123456789' })
     trackingId: string;
 
     @IsPhoneNumber('EG')
     @IsNotEmpty()
+    @ApiProperty({ example: '0123456789' })
     phoneNumber: string;
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({ example: 'Electronics delivery' })
     description: string;
 }
